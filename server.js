f38bc3561a9214f1a4e1c686c014931e2622fdb9
Steven Harradine <stevenharradine@gmail.com>
2018-05-08 01:20:31 -0400

    sanitize data input

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const wireExec = wirePath + "wircmd"
 
 http.createServer(function (req, res) {
 	html = "<html><head><meta name='viewport' content='width=device-width, initial-scale=1'></head><body>"
-	unsanitized_command = req.url.split("=")[1]
+	sanitized_command = sanitize_buttons (req.url.split("=")[1])
 
-	exec(wireExec + ' -i 192.168.2.91 -c 3 -b "' + unsanitized_command + '"', (error, stdout, stderr) => {
+	exec(wireExec + ' -i 192.168.2.91 -c 3 -b "' + sanitized_command + '"', (error, stdout, stderr) => {
 		if (error) {
 			html += `exec error: ${error}`;
 
@@ -26,3 +26,13 @@ http.createServer(function (req, res) {
 		res.end(); //end the response
 	});
 }).listen(8080);
+
+function sanitize_buttons (unsanitize_string) {
+	var VALID_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,"
+	var sanitize_string = ""
+	for (var i = 0; i < unsanitize_string.length; i++) {
+		curTestChar = unsanitize_string.charAt(i)
+		sanitize_string += VALID_CHAR.indexOf(curTestChar) >= 0 ? curTestChar : ''
+	}
+	return sanitize_string
+}
